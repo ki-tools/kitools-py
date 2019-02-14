221b99601184bbfd333da052aefba240e2eb13ef
Patrick <pcstout@gmail.com>
2019-02-14 07:15:11 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kitools",
+    version="0.0b1",
+    author="Patrick Stout",
+    author_email="pstout@prevagroup.com",
+    license="Apache2",
+    description="ki tools package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ki-tools/kitools-py",
+    packages=setuptools.find_packages(exclude=['tests*']),
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"
+    ),
+    install_requires=[
+        'synapseclient==1.9.0'
+    ]
+)
---
+++ src/__init__.py
@@ -0,0 +1,31 @@
+# Copyright 2018-present, Bill & Melinda Gates Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+name = 'kitools'
+
+
+def create_analysis(path):
+    return 'create_analysis: {0}'.format(path)
+
+
+def data_load(handle):
+    return 'data_load: {0}'.format(handle)
+
+
+def data_pull(handle):
+    return 'data_pull: {0}'.format(handle)
+
+
+def data_save(data, filename):
+    return 'data_save: {0}'.format(filename)
